aaab43420b8ec3e789417ccd7b78a69624bb3080
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-04-21 11:18:52 -0600

    Initial Commit

---
+++ App_Prueba_H/App_Prueba_HApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_Prueba_HApp.swift
+//  App_Prueba_H
+//
+//  Created by Oscar Roberto Valdes Rodriguez on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct App_Prueba_HApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App_Prueba_HUITests/App_Prueba_HUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  App_Prueba_HUITestsLaunchTests.swift
+//  App_Prueba_HUITests
+//
+//  Created by Oscar Roberto Valdes Rodriguez on 21/04/23.
+//
+
+import XCTest
+
+final class App_Prueba_HUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
